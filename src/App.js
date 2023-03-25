2e4f0ac533e411cb51cb7098cff2d3ffd43910c4
Robson Egute <eguterobson@yahoo.com>
2023-03-25 03:41:28 +0000

    Installed marked library

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import Marked from "marked";
+
+class Markdown extends React.Component {
+constructor(props) {
+  super(props);
+  this.state = {
+    input: ""
+  }
+  this.handleChange = this.handleChange.bind(this);
+}
+
+handleChange(event) {
+  this.setState({
+    input: event.target.value
+  })
+}
+
+render() {
+  return (
+    <div>
+      <h1>Markdown Previewer</h1>
+      <textarea id="editor" onChange={this.handleChange}> </textarea>
+      <div id="preview">{this.state.input}</div>
+    </div>
+  )
+}
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Markdown />
     </div>
   );
 }
