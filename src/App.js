33b02ee9768f051f16092cd7a81468b61211cd9a
Robson Egute <eguterobson@yahoo.com>
2023-03-28 03:13:46 +0100

    Project completed and @media querry added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import './App.css';
 import React from "react";
 import {marked} from "marked";
-
+/*
+const editorStyle = {
+  display: "inline-block",
+  position: "absolute",
+  top: "0px",
+  left: "0px",
+  width: "50%",
+  height: "600px",
+  borderLeft: "3px solid blue",
+  borderRight: "3px solid blue",
+  borderBottom: "3px solid blue"
+}*/
 //a header (H1 size), a sub header (H2 size), a link, inline code, 
 //a code block, a list item, a blockquote, an image, and bolded text
+
+marked.setOptions({
+  breaks: true
+})
 class MarkdownComponent extends React.Component {
 constructor(props) {
   super(props);
@@ -38,9 +53,11 @@ constructor(props) {
 **bold text**  
 
 [title](https://www.example.com)
-    `
+    `,
+    visibility: true
   };
   this.handleChange = this.handleChange.bind(this);
+  this.handleClick = this.handleClick.bind(this);
 }
 
 handleChange(event) {
@@ -48,16 +65,46 @@ handleChange(event) {
     input: event.target.value
   })
 }
+handleClick(event) {
+  event.preventDefault()
+  this.setState(state => {
+    if (state.visibility === true) {
+      return {visibility: false}
+    } else {
+      return {visibility: true}
+    }
+  })
+};
 
 render() {
-  
-  return (
-    <div>
+  if (this.state.visibility === true) {
+    return (
+<div className="container1">
       <h1>Markdown Previewer</h1>
-      <textarea id="editor" onChange={this.handleChange} value={this.state.input}> </textarea>
-      <div id="preview" inputText={this.state.input} dangerouslySetInnerHTML={{ __html: marked(this.state.input)}} />
-    </div>
-  )
+        <div className="headerContainer1">
+          <div className="header1">
+            <button className="toggle1" type="button" onClick={this.handleClick}></button>
+          </div>
+        <textarea id="editor" onChange={this.handleChange} value={this.state.input}> </textarea>
+      </div>
+  <div className="previewContainer1">
+    <div className="pheader"></div>
+  <div id="preview" inputText={this.state.input} dangerouslySetInnerHTML={{ __html: marked(this.state.input)}} />
+  </div>
+</div>
+    )
+  } else {
+    return (
+      <div className="container2">
+      <h1>Markdown Previewer</h1>
+        <div className="header2">
+          <button className="toggle2" onClick={this.handleClick}></button>
+          <div className="previewBlock2" id="previewBlock2" inputText={this.state.input} dangerouslySetInnerHTML={{ __html: marked(this.state.input)}} />
+        </div>
+      </div>
+    )
+  }
+  
 }
 }
 
